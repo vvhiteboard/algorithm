24988d4f7a16eabf8a86d099f753e385b12f5759
vvhiteboard <vvhiteboard@gmail.com>
2018-04-23 14:59:32 +0900

    1. 순회 메소드가 BSTNode가 아닌 BST에 존재하도록 변경 2. BSTNode 클래스를 BST 안에서 정의

--- BSTOrder/BST.py
+++ BSTOrder/BST.py
@@ -1,78 +1,57 @@
 from collections import deque
 
-class BSTNode:
-    parent = None
-    left = None
-    right = None
-
-    element = None
-
-    def __init__(self, parent, element):
-        self.parent = parent
-        self.element = element
-
-        if element is not None:
-            self.left = BSTNode(self, None)
-            self.right = BSTNode(self, None)
-
-    def is_external(self):
-        if self.element is None:
-            return True
-        return False
-
-    def is_internal(self):
-        if self.element is not None:
-            return True
-        return False
-
-    def is_root(self):
-        if self.parent is None:
-            return True
-        return False
-
-    def is_right(self):
-        if self.is_root():
-            return False
-        elif self.parent.right == self:
-            return True
-        return False
-
-    def is_left(self):
-        if self.is_root():
-            return False
-        elif self.parent.left == self:
-            return True
-        return False
-
-    def do_pre_order(self):
-        print(self.element)
-
-        if self.left.is_internal():
-            self.left.do_pre_order()
-
-        if self.right.is_internal():
-            self.right.do_pre_order()
-
-    def do_post_order(self):
-        if self.left.is_internal():
-            self.left.do_post_order()
-
-        if self.right.is_internal():
-            self.right.do_post_order()
-
-        print(self.element)
-
-    def do_in_order(self):
-        if self.left.is_internal():
-            self.left.do_in_order()
-
-        print(self.element)
-
-        if self.right.is_internal():
-            self.right.do_in_order()
-
 
 class BST:
+    class BSTNode:
+        parent = None
+        left = None
+        right = None
+
+        element = None
+
+        def __init__(self, parent, element):
+            self.parent = parent
+            self.element = element
+
+            if element is not None:
+                self.left = BST.BSTNode(self, None)
+                self.right = BST.BSTNode(self, None)
+
+        def is_external(self):
+            if self.element is None:
+                return True
+            return False
+
+        def is_internal(self):
+            if self.element is not None:
+                return True
+            return False
+
+        def is_root(self):
+            if self.parent is None:
+                return True
+            return False
+
+        def is_right(self):
+            if self.is_root():
+                return False
+            elif self.parent.right == self:
+                return True
+            return False
+
+        def is_left(self):
+            if self.is_root():
+                return False
+            elif self.parent.left == self:
+                return True
+            return False
+
+        def visit(self):
+            print(self.element)
+
+
+
+
     root = BSTNode(None, None)
 
     def get_root(self):
@@ -80,7 +59,7 @@ class BST:
 
     def add_node(self, element):
         if self.root.is_external():
-            self.root = BSTNode(None, element)
+            self.root = BST.BSTNode(None, element)
 
         else:
             node = self.search_node(element)
@@ -89,18 +68,45 @@ class BST:
                 return
 
             if node.is_right():
-                node.parent.right = BSTNode(node.parent, element)
+                node.parent.right = BST.BSTNode(node.parent, element)
             else:
-                node.parent.left = BSTNode(node.parent, element)
+                node.parent.left = BST.BSTNode(node.parent, element)
 
     def pre_order(self):
-        self.root.do_pre_order()
+        self.do_pre_order(self.root)
+
+    def do_pre_order(self, node):
+        node.visit()
+
+        if node.left.is_internal():
+            self.do_pre_order(node.left)
+
+        if node.right.is_internal():
+            self.do_pre_order(node.right)
 
     def post_order(self):
-        self.root.do_post_order()
+        self.do_post_order(self.root)
+
+    def do_post_order(self, node):
+        if node.left.is_internal():
+            self.do_post_order(node.left)
+
+        if node.right.is_internal():
+            self.do_post_order(node.right)
+
+        node.visit()
 
     def in_order(self):
-        self.root.do_in_order()
+        self.do_in_order(self.root)
+
+    def do_in_order(self, node):
+        if node.left.is_internal():
+            self.do_in_order(node.left)
+
+        node.visit()
+
+        if node.right.is_internal():
+            self.do_in_order(node.right)
 
     def level_order(self):
         queue = deque()
--- BSTOrder/main.py
+++ BSTOrder/main.py
@@ -1,7 +1,6 @@
 from BST import BST
 
 if __name__ == "__main__":
-    print("tree-init-start")
     bst = BST()
     bst.add_node("D")
     bst.add_node("B")
@@ -10,8 +9,7 @@ if __name__ == "__main__":
     bst.add_node("C")
     bst.add_node("E")
     bst.add_node("G")
-    print("""result is \nPre : D B A C F E G\nPost : A C B E G F D\nIn : A B C D E F G\nlevel : D B F A C E G""")
-    print("tree-init-end")
+    print("tree initialize complete")
 
     print("=========== Pre-Order ==========")
     bst.pre_order()
