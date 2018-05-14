b35937d414caa597ebe67aacaa7692d8862aa540
vvhiteboard <vvhiteboard@gmail.com>
2018-05-14 20:23:08 +0900

    [알고리즘] 로또 - ACMICPC 6603번 문제 - 로또

---
+++ rotto/rotto.py
@@ -0,0 +1,28 @@
+def get_user_input() :
+    user_input = input()
+    inputs = user_input.split()
+
+    return int(inputs[0]), inputs[1:]
+
+def run(k, arr):
+    rotto(k, arr, [], 0, 0, k)
+
+def rotto(k, arr, result, depth, start, end):
+    if depth == 6:
+        print(" ".join(result))
+        return
+
+    for index in range(start, end + (depth - 5)):
+        result.append(arr[index])
+        rotto(k, arr, result, depth + 1, index + 1, end)
+        result.pop()
+
+if __name__ == "__main__":
+    while True:
+        k, arr = get_user_input()
+
+        if k is 0:
+            break
+
+        run(k, arr)
+        print("")
